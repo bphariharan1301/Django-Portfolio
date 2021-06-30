2e5e273d9c4e37f3da4a2af1f725afdeb9f318bb
HARIHARAN B P <bphariharan1301@gmail.com>
2021-06-30 17:20:40 +0530

    Heroku Config Added

--- Portfolio_Website/settings.py
+++ Portfolio_Website/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = os.environ.get('DEBUG')
+# DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 
 ALLOWED_HOSTS = ['*']
