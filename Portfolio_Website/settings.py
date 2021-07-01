ed5e8ef6154926fda960f72b65edf100f5a815ee
HARIHARAN B P <bphariharan1301@gmail.com>
2021-07-01 14:43:36 +0530

    Minor Bugs Fixed after deployment

--- Portfolio_Website/settings.py
+++ Portfolio_Website/settings.py
@@ -27,14 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-# SECRET_KEY = 'django-insecure-dh7^%&-8$bqipl_%j@^_(+nh2h#$$6xiv3qx6agirvcm*#jb*s'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DEBUG')
-# DEBUG = True
 
 
 ALLOWED_HOSTS = ['*', 'localhost']
