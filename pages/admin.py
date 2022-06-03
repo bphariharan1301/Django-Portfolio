48a8a3adc9c396ae4a4efbb5c468c7cb01bb4444
HARIHARAN B P <bphariharan1301@gmail.com>
2022-06-03 13:22:22 +0530

    Change: Export Added

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,6 @@
 
 from django.contrib import admin
+from import_export.admin import ExportActionMixin
 from .models import *
 
 # Register your models here.
@@ -8,4 +9,11 @@ admin.site.register(Work)
 # admin.site.register(User)
 admin.site.register(Testimonail)
 admin.site.register(Technical_Skill)
-admin.site.register(Contact)
+
+class ContactAdmin(ExportActionMixin, admin.ModelAdmin):
+  list_display = ('name', 'email', 'subject', 'phone_number', 'message')
+
+# class BookAdmin(ExportActionMixin, admin.ModelAdmin):
+#     list_display = ('title', 'description', 'author', 'year')
+
+admin.site.register(Contact, ContactAdmin)
